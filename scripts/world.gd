4faf08d425091968a084b2fee2f11b67c73063be
Alessandro <alessandro.mamusa@nextroll.com>
2023-11-11 15:18:19 +0100

    test world

---
+++ scripts/world.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Engine.time_scale = 10
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Player/Label.text = str(Time.get_ticks_msec()*Engine.time_scale/1000.0)
