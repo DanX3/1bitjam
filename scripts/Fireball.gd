1d53af3a5d0d2a9cff9edd877c8a0d7ffa783828
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2023-11-12 09:33:54 +0100

    fixed fireball exiting from the screen

--- scripts/Fireball.gd
+++ scripts/Fireball.gd
@@ -16,14 +16,5 @@ func _physics_process(delta):
 		if collision.get_collider().name == caravan.name:
 			queue_free()
 
-func destroy():
-	if $Particles1:
-		$Particles1.emitting = false
-		get_node("/root/UtilsInstance").destroy_after($Particles1, get_parent(), 1.1)
-	if $Particles2:
-		$Particles2.emitting = false
-		get_node("/root/UtilsInstance").destroy_after($Particles2, get_parent(), 1.1)
-	queue_free()
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	destroy()
+	queue_free()
